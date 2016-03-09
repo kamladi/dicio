38caca0727d1c2c730d826ed459bbc75126d2b4e
Kedar Amladi <kamladi@gmail.com>
2016-03-09 15:40:57 -0500

    fixed mongodb port

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 const DB_URL =
     process.env.MONGOLAB_URI ||
     process.env.MONGOHQ_URL ||
-    'mongodb://localhost:27017/diccio';
+    'mongodb://127.0.0.1:27017/diccio';
 
 exports.DB_URL = DB_URL;
 
