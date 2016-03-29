eab8cf2d5708a99a7f57bb36002e760caf16158d
Kedar Amladi <kamladi@gmail.com>
2016-03-29 16:15:14 -0400

    updated sensor data to query by mac address

--- server/models/SensorRecord.js
+++ server/models/SensorRecord.js
@@ -4,11 +4,11 @@ var ObjectId = mongoose.Schema.ObjectId;
 // TODO: Add property for outlet's MAC address
 var sensorRecordSchema = new mongoose.Schema({
 	timestamp: {type: Date, default: Date.now},
-	outlet_id: {type: ObjectId, required: true},
+	mac_address: {type: String, required: true},
 	cur_temperature: Number,
 	cur_humidity: Number,
 	cur_light: Number,
 	cur_power: Number
-}, {collection: 'sensor_record'});
+}, {collection: 'sensor_records'});
 
 module.exports = mongoose.model('SensorRecord', sensorRecordSchema);
