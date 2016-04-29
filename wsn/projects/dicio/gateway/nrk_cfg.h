7462887aa670635867d141290adf960fa34be946
dansantorojr <daniel.d.santoro@gmail.com>
2016-04-28 21:40:20 -0400

    small updates.

--- wsn/projects/dicio/gateway/nrk_cfg.h
+++ wsn/projects/dicio/gateway/nrk_cfg.h
@@ -51,10 +51,9 @@
 #define NRK_MAX_TASKS       		    10
 #define	NRK_N_RES			            1	
                            
-#define NRK_TASK_IDLE_STK_SIZE         256   // Idle task stack size min=32 
-//#define NRK_APP_STACKSIZE              512 
-#define NRK_APP_STACKSIZE				256 //512
-#define NRK_KERNEL_STACKSIZE           256 
+#define NRK_TASK_IDLE_STK_SIZE  256   // Idle task stack size min=32 
+#define NRK_APP_STACKSIZE		256
+#define NRK_KERNEL_STACKSIZE    256 
 
 // number of semaphores in the system!
 #define NRK_MAX_RESOURCE_CNT           10
